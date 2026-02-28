951c0ad51ab87acf9d3620bed6c7bcefefdd33dd
yaduykp123 <yaduykp123@gmail.com>
2026-02-28 11:57:23 +0530

    Time: 69 ms (10.91%), Space: 55.9 MB (22.86%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+ 
+ for(i=0;i<=nums.length-1;i++){
+    for(j=i+1;j<=nums.length-1;j++){
+        if(nums[i]+nums[j] === target){
+            return [i,j]
+        }
+    }
+ }
+   
+};
+
+
+twoSum([2,7,11,15],9)
