c4aac5bf4d98ef5f4d1b2535efeb8d56fc6fa944
yaduykp123 <yaduykp123@gmail.com>
2026-02-28 11:58:58 +0530

    Time: 0 ms (100%), Space: 53.5 MB (66.13%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+
+  for (let i = digits.length - 1; i >= 0; i--) {
+
+    
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+   
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+
+console.log(plusOne([1,2,3]))
