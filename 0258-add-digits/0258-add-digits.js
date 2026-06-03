ac6d7ea1be04d9fd3cd62db70e5c0e36f2e22b4f
yaduykp123 <yaduykp123@gmail.com>
2026-06-03 12:48:41 +0530

    Time: 2 ms (16.4%), Space: 57.5 MB (19.24%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -3,12 +3,10 @@
  * @return {number}
  */
 var addDigits = function(num) {
-   
-    let result
+
     for(i=1;i<=num;i++){
      num = num.toString().split('').reduce((acc , data) => acc + Number(data),0)
 
     }
-    return result = num
-        
+    return result = num     
 };
